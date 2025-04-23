f49f8e4cb2af9b8490861b20ce4cc2043fcd7acf
KacDabQ <u118_kacdab_waw@technischools.com>
2025-04-23 13:37:06 +0200

    Filter cities

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -4,8 +4,10 @@ import Item from "@/components/event/item";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import EventI from "@/types/event";
+import { set } from "mongoose";
 
 export default function Front() {
+  const [filteredEvents, setFilteredEvents] = useState([]);
   const [events, setEvents] = useState([]);
   const getEvents = async () => {
     const res = await axios.get("/api/dashboard/get-events");
@@ -13,6 +15,26 @@ export default function Front() {
     console.log(res.data.events);
 
     setEvents(res.data.events);
+    setFilteredEvents(res.data.events);
+  };
+
+  const cities = [
+    "Warszawa",
+    "Kraków",
+    "Wrocław",
+    "Poznań",
+    "Gdańsk",
+    "Szczecin",
+    "Lublin",
+    "Katowice",
+  ];
+
+  const filterEventsByCity = (city: string) => {
+    if (city === "all") {
+      setFilteredEvents(events);
+      return;
+    }
+    setFilteredEvents(events.filter((event: EventI) => event.city === city));
   };
 
   useEffect(() => {
@@ -21,7 +43,25 @@ export default function Front() {
 
   return (
     <div className="mt-2">
-      {events.map((event: EventI) => (
+      <div>
+        <select
+          name="city-filter"
+          id="city-filter"
+          className="p-5"
+          onChange={(e) => {
+            const selectedCity = e.target.value;
+            filterEventsByCity(selectedCity);
+          }}
+        >
+          <option value="all">Wszystkie</option>
+          {cities.map((city) => (
+            <option key={city} value={city} className="text-black">
+              {city}
+            </option>
+          ))}
+        </select>
+      </div>
+      {filteredEvents.map((event: EventI) => (
         <Item key={event._id} event={event} />
       ))}
     </div>
